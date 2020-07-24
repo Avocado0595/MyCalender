81d28fb9f1e0ffc2b2b99026b1d7f1851326611e
Tran Thanh Xuan <ttxuan0595@gmail.com>
2020-07-24 21:22:10 +0700

    Initial commit

---
+++ Ajob.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyCalender
+{
+    /// <summary>
+    /// Interaction logic for Ajob.xaml
+    /// </summary>
+    public partial class Ajob : UserControl
+    {
+
+        private PlanItem job;
+        public Ajob(PlanItem job)
+        {
+            InitializeComponent();
+            this.Job = job;
+            cbxStatus.ItemsSource = PlanItem.listStatus;
+            showData();
+        }
+
+        public PlanItem Job { get => job; set => job = value; }
+
+        private event EventHandler edit;
+        public event EventHandler Edit
+        {
+            add { edit += value; }
+            remove { edit -= value; }
+        }
+
+        private event EventHandler delete;
+        public event EventHandler Delete
+        {
+            add { delete += value; }
+            remove { delete -= value; }
+        }
+
+
+        void showData()
+        {
+            tbxJob.Text = Job.Job;
+            tpkStart.SelectedTime = new DateTime(1,1,1,(int)Job.FromTime.X,(int)Job.FromTime.Y,0);
+            tpkEnd.SelectedTime = new DateTime(1, 1, 1,(int) Job.ToTime.X, (int)Job.ToTime.Y, 0);
+            cbxStatus.SelectedIndex = PlanItem.listStatus.IndexOf(Job.Status);
+            ckbDone.IsChecked = PlanItem.listStatus.IndexOf(Job.Status) == (int)EPlanItem.DONE;
+        }
+
+        private void btnEdit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Job.Job = tbxJob.Text;
+            Job.FromTime = new Point(tpkStart.SelectedTime.Value.Hour, tpkStart.SelectedTime.Value.Minute);
+            Job.ToTime = new Point(tpkEnd.SelectedTime.Value.Hour, tpkEnd.SelectedTime.Value.Minute);
+            Job.Status = cbxStatus.SelectedItem == null ? "DOING": cbxStatus.SelectedItem.ToString();
+
+            if (edit != null)
+                edit(this, new EventArgs());
+        }
+
+        private void btnDelete_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (delete != null)
+                delete(this, new EventArgs());
+        }
+
+        private void ckbDone_Click(object sender, RoutedEventArgs e)
+        {
+            cbxStatus.SelectedIndex = (bool)ckbDone.IsChecked ? (int)EPlanItem.DONE : (int)EPlanItem.DOING;
+        }
+
+        private void cbxStatus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ckbDone.IsChecked = cbxStatus.SelectedIndex == (int)EPlanItem.DONE;
+        }
+    }
+}
---
+++ DailyPlan.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyCalender
+{
+    /// <summary>
+    /// Interaction logic for DailyPlan.xaml
+    /// </summary>
+    public partial class DailyPlan : Window
+    {
+        private DateTime date;
+        private PlanData job;
+        public DateTime Date { get => date; set => date = value; }
+        public PlanData Job { get => job; set => job = value; }
+
+        public DailyPlan(DateTime date, PlanData job)
+        {
+            InitializeComponent();
+            this.Date = date;
+            this.Job = job;
+            dtpSelectionDate.SelectedDate = Date;
+        }
+
+        private void ShowJobByDate(DateTime date)
+        {
+            spnlPlans.Children.Clear();
+            if (Job != null && Job.Job != null)
+            {
+                for (int i = 0; i < Job.Job.Count; i++)
+                {
+                    if (Job.Job[i].Date.Date == date.Date)
+                    {
+                        Addjob(Job.Job[i]);
+                    }
+                }
+            }
+        }
+        void Addjob(PlanItem job)
+        {
+            Ajob ajob = new Ajob(job);
+            ajob.Edit += Ajob_Edit;
+            ajob.Delete += Ajob_Delete;
+            spnlPlans.Children.Add(ajob);
+        }
+        private void Ajob_Delete(object sender, EventArgs e)
+        {
+            Ajob uc = sender as Ajob;
+            PlanItem job = uc.Job;
+            
+            spnlPlans.Children.Remove(uc);
+            Job.Job.Remove(job);
+        }
+
+        private void Ajob_Edit(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dtpSelectionDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowJobByDate((sender as DatePicker).SelectedDate.Value);
+        }
+
+        private void btnPrevday_Click(object sender, RoutedEventArgs e)
+        {
+            dtpSelectionDate.SelectedDate = dtpSelectionDate.SelectedDate.Value.AddDays(-1);
+        }
+
+        private void btnNextday_Click(object sender, RoutedEventArgs e)
+        {
+            dtpSelectionDate.SelectedDate = dtpSelectionDate.SelectedDate.Value.AddDays(1);
+        }
+
+        private void MenuItemToday_Click(object sender, RoutedEventArgs e)
+        {
+            dtpSelectionDate.SelectedDate = DateTime.Now;
+        }
+
+        private void MenuItemAddJob_Click(object sender, RoutedEventArgs e)
+        {
+
+            PlanItem item = new PlanItem() { Date = dtpSelectionDate.SelectedDate.Value, FromTime = new Point(0,0), ToTime=new Point(0,0), Job = "", Status="COMING" };
+            Job.Job.Add(item);
+            Addjob(item);
+        }
+    }
+}
